06f31d100b7f9eea4673cb9f59b1c88555c17a1a
Danylo.Nabok <nabok.danylo@gmail.com>
2020-12-28 17:00:32 +0200

    Completed first task

---
+++ task-1/script.js
@@ -0,0 +1,28 @@
+function submitForm() {
+    let firstNumber = Number(document.getElementById('firstNumber').value);
+    let secondNumber = Number(document.getElementById('secondNumber').value);
+
+    let errorElement = document.getElementById('error');
+    errorElement.innerHTML = '';
+
+    let resultElement = document.getElementById('result');
+    resultElement.innerHTML = '';
+
+    let result = [];
+
+    if (firstNumber === secondNumber || firstNumber+1 === secondNumber || firstNumber === secondNumber+1) {
+        errorElement.innerHTML = 'Enter valid numbers pls';
+    } else if (firstNumber < secondNumber) {
+        for (let i = firstNumber+1; i < secondNumber; i++) {
+            result.push(i);
+        }
+        resultElement.innerHTML = `Digits between ${firstNumber} and ${secondNumber} ${ result.length > 1 ? 'are' : 'is' }: ${result.join(', ')}`;
+    } else if (firstNumber > secondNumber) {
+        for (let i = secondNumber+1; i < firstNumber; i++) {
+            result.push(i);
+        }
+        resultElement.innerHTML = `Digits between ${secondNumber} and ${firstNumber} ${ result.length > 1 ? 'are' : 'is' }: ${result.join(', ')}`;
+    } else {
+        errorElement.innerText = 'Something went wrong, please try again.'
+    }
+}

fc5ea4a9c9f2c162d30aacdccfd033163603ea35
Danylo.Nabok <nabok.danylo@gmail.com>
2021-01-25 15:51:47 +0200

    Completed 4 task.

---
+++ Task 4/script.js
@@ -0,0 +1,33 @@
+const assert = require('assert');
+
+function extendWithEndless(...params) {
+    let result = {};
+
+    params.map((item) => {
+       result = Object.assign(result, item);
+    });
+
+    return result;
+}
+
+
+
+assert.deepStrictEqual(extendWithEndless(
+    { flatWhite: ['doppio', 'hot', 'milk'], isValid: true },
+    { isValid: false,
+        additionalProp: { thisIsGoodProp: 123 } },
+    { prop3: true },
+    { prop4: true },
+    { isValid: [false, false] },
+    ),
+
+    {
+        flatWhite: ['doppio', 'hot', 'milk'],
+        isValid: [false, false],
+        additionalProp: { thisIsGoodProp: 123 },
+        prop3: true,
+        prop4: true
+    }
+);
+
+console.log('Looks good');
